58500d136ad971cb197614e87c4d33e7130bc952
Michał Wielich <poiu.m.w@gmail.com>
2017-03-26 11:41:02 +0200

    navbar generation in a loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :get_navbar
+
+  def get_navbar
+    @navbar = [
+      {
+        :name => 'Desktop',
+        :path => desktop_path,
+        :controller_name => 'desktop',
+      },{
+        :name => 'Characters',
+        :path => characters_path,
+        :controller_name => 'characters',
+      },{
+        :name => 'Reports',
+        :path => reports_path,
+        :controller_name => 'reports',
+      },
+    ]
+  end
 
   protected
     def add_breadcrumb name, url = ''
